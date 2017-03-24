8f4fcd9e86ffee64ba749ac94f8961832958ca9c
Tóth Csaba <csaba9308@gmail.com>
2017-03-24 10:50:45 +0100

    NoteDetailsActivity content view fix

--- app/src/main/java/com/github/tothcs/ui/notedetails/NoteDetailsActivity.java
+++ app/src/main/java/com/github/tothcs/ui/notedetails/NoteDetailsActivity.java
@@ -17,7 +17,7 @@ public class NoteDetailsActivity extends AppCompatActivity implements NoteDetail
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_note_details);
 
         NotesApplication.injector.inject(this);
     }
